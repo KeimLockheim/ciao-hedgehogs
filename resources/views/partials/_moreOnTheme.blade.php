0655a297467b3a1bf017dcaaddcc2c72c3c6c378
xavmer9 <xavier.mercier@heig-vd.ch>
2016-06-09 17:59:06 +0200

    add partials

---
+++ resources/views/partials/_moreOnTheme.blade.php
@@ -0,0 +1,6 @@
+<h3 class="titreBox">Sur le thème {{$domain->name}} :</h3>
+
+<a href="/domain/{{$domain->id}}/questions"><button type="button" class="btn btn-xs">Questions / Réponses</button></a>
+<a href="/domain/{{$domain->id}}/discussions"><button type="button" class="btn btn-xs">Discussion entre jeunes</button></a>
+<a href="/domain/{{$domain->id}}/temoignages"><button type="button" class="btn btn-xs">Témoignages</button></a>
+<a href="/domain/{{$domain->id}}/more"><button type="button" class="btn btn-xs">Plus d'outils</button></a>
