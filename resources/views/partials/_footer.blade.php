c2cad27e3b8977b9c998872a3d6470c439c8e173
emili3 <emiliegachoud@MBP-de-Emilie-2.domain_not_set.invalid>
2016-06-12 15:31:03 +0200

    viewMaster, menu + rooter dans partials

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,1 +1,82 @@
-add include('partial._footer') in view_master
+<footer>
+
+			<div class="fondFooter">
+
+				<div class="container">
+					<div class="row">
+						<ul>
+							<div class="col-md-2">
+								<li><a href="https://www.facebook.com/ciao.ch"><img src="{{ asset('assets/img/facebook.png') }}" alt="Facebook" id="facebook"></a></li>
+							</div>
+							<div class="col-md-2">
+								<li><a href="#">Ton avis</a></li>
+							</div>
+							<div class="col-md-2">
+								<li><a href="#">Toutes les questions</a></li>
+							</div>
+							<div class="col-md-2">
+								<li><a href="#">À propos de Ciao</a></li>
+							</div>
+							<div class="col-md-2">
+								<li><a href="#">Dictionnaire</a></li>
+							</div>
+						</ul>
+
+					</div>
+				</div>
+			</div>
+
+				<div class="container designBoxFooter">
+
+					<div class="row">
+
+						<div class="col-md-4 footerCenter">
+							<div class="row">
+								<div class="col-md-12">
+									<h3>Avec le soutien de :</h3>
+								</div>
+								<div class="col-md-6">
+									<a href="http://oakfnd.org/"><img src="{{ asset('assets/img/oak.png') }}" class="logoFooter" alt="Oak Foundation"></a>
+								</div>
+								<div class="col-md-6">
+									<a href="https://www.loro.ch/fr"><img src="{{ asset('assets/img/romande.jpg') }}" class="logoFooter" alt="Loterie Romande"></a>
+								</div>	
+								<div class="col-md-12 iban">
+
+									<h4>Soutenez nous :</h4>
+									<h5>CCP 10-5261-6</h5>
+								</div>								
+							</div>
+					  	</div>
+
+					  	<div class="col-md-4">
+
+							  		<h3 class="footerCenter">Numéros d'urgence :</h3>
+
+							  		<p class="footerCenter"><img src="{{ asset('assets/img/medecin.png') }}" alt="medecin" class="footerIcons"> Médical - <span class="footerNum">144</span></p>
+							  		<p class="footerCenter"><img src="{{ asset('assets/img/pompier.png') }}" alt="pompier" class="footerIcons"> Pompiers - <span class="footerNum">118</span></p>
+							  		<p class="footerCenter"><img src="{{ asset('assets/img/police.png') }}" alt="police" class="footerIcons"> Police - <span class="footerNum">117</span></p>
+							  		<p class="footerCenter"><img src="{{ asset('assets/img/general.png') }}" alt="general" class="footerIcons"> Général - <span class="footerNum">112</span></p>
+							
+						</div>
+
+					  	<div class="col-md-4">
+
+					  		<h3 class="footerCenter">Ciao c'est :</h3>
+					  		<ul>
+						  		<li>Une association</li>
+						  		<li>Un site internet créé en 1997, certifié accessible et HON</li>
+						  		<li>Une collaboration avec 15 institutions partenaires</li>
+						  		<li>Des sites partenaires : feelok.ch et tschau.ch</li>
+						  		<li>En 2015 plus de 13 000 visiteurs par semaine en moyenne, 2'000'000 de pages vues et 2848 questions posées</li>
+					  		</ul>
+							
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</footer>
--- resources/views/partials/_megamenu.blade.php
+++ resources/views/partials/_megamenu.blade.php
@@ -1,1 +1,202 @@
-add include('partial._megamenu') in view_master
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+
+                <ul class="nav navbar-nav">
+
+                    <li><a><img src="img/home.png" alt="home" class="imgNav"><p>Accueil</p></a></li> 
+                    <!------------------------>
+
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/sante.png') }}" alt="sante" class="imgNav"><p>Santé <b class="caret"></b></p> </a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Santé</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">État de santé</a></li>
+                                    <li><a href="#">Où, qui consulter?</a></li>
+                                    <li><a href="#">Apparence, de l'importance</a></li>
+                                    <li><a href="#">Piercing, tatouage, épiler</a></li>
+                                    <li><a href="#">Puberté chez les garçons</a></li>
+                                    <li><a href="#">Puberté chez les filles</a></li>
+                                    <li><a href="#">Musique et décibels</a></li>
+                                    <li><a href="#">Fatigue et sommeil</a></li>
+                                    <li><a href="#">Infections et maladies</a></li>
+                                    <li><a href="#">Dépression et suicide</a></li>
+                                    <li><a href="#">Santé mentale</a></li>
+                                    <li><a href="#">Santé et Internet</a></li>
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Stress</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Qu'est-ce que c'est?</a></li>
+                                    <li><a href="#">Les mécanismes</a></li>
+                                    <li><a href="#">Réponses au stress</a></li>
+                                    <li><a href="#">Symptômes</a></li>
+                                    <li><a href="#">Les sources</a></li>
+                                    <li><a href="#">Y faire face</a></li>
+                                    <li><a href="#">Stress et someil</a></li>
+                                    <li><a href="#">Stress et examens</a></li>
+                                    <li><a href="#">Stress et puberté</a></li>
+                                    <li><a href="#">Stress et quotidien</a></li>
+                                    <li><a href="#">Stress et vie scolaire</a></li>
+                                    <li><a href="#">Ressources contre le stress</a></li>
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Boire, fumer, se droguer</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Les drogues</a></li>
+                                    <li><a href="#">Les produits</a></li>
+                                    <li><a href="#">La consommation</a></li>
+                                    <li><a href="#">L'entourage</a></li>
+                                    <li><a href="#">La société</a></li>
+                                    <li><a href="#">Boire, fumer... et estime de soi</a></li>
+                                    <li><a href="#">Trop d'Internet</a></li>
+                                    <li><a href="#">Jeux de hasard</a></li>			
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Manger-bouger</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Poids adolescence</a></li>
+                                    <li><a href="#">Manger, comment?</a></li>
+                                    <li><a href="#">Maigrir, grossir</a></li>
+                                    <li><a href="#">Anorexie-boulimie</a></li>
+                                    <li><a href="#">Comment bouger?</a></li>
+                                    <li><a href="#">Bouger pour sa santé</a></li>
+                                    <li><a href="#">Bouger, c'est nul...</a></li>
+                                    <li><a href="#">Bouger, par plaisir</a></li>			
+                                    <li><a href="#">Bouger, sans risque</a></li>			
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </li>
+
+
+                    <!------------------------>
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/moi.png') }}" alt="moi, les autres" class="imgNav"><p>Moi, toi,...<b class="caret"></b></p></a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Estime de soi</li>
+                                    <li class="divider"></li>
+
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Moi, toi et les autres</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/sexualite.png') }}" alt="Sexualité" class="imgNav"><p>Sexualité <b class="caret"></b></p></a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Sexualité</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/violence.png') }}" alt="Violences" class="imgNav"><p>Violences <b class="caret"></b></p></a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Violences</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Discrimination et racismes</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/religion.png') }}" alt="Religions" class="imgNav"><p>Religions <b class="caret"></b></p></a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Religions</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+
+                    <li class="dropdown menu-large">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/work.png') }}" alt="Avenir" class="imgNav"><p>Avenir<b class="caret"></b></p></a>				
+                        <ul class="dropdown-menu megamenu row">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Argent</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header titreMegaMenu">Formation et travail</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">BlaBla</a></li>
+
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>                
+
+
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><img src="{{ asset('assets/img/sign_in.png') }}" alt="login" class="imgNav">Connexion</a></li>
+                </ul>
+            </div>
+        </nav>
