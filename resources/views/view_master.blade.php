e45023c1e74617931fbfb36ffd3d795411eff1bf
emili3 <emiliegachoud@mbp-de-emilie-2.einet.ad.eivd.ch>
2016-06-13 15:46:15 +0200

    header

--- resources/views/view_master.blade.php
+++ resources/views/view_master.blade.php
@@ -47,13 +47,10 @@
                     <a href="/urgences"><img src="{{ asset('assets/img/ambulance-xxl.png') }}" alt="urgence" class="imgHeader"></a>
                          <a href="/ask/1"><button type="submit" class="btn btn-xs large">Poser ma question!</button></a>
 
-						<img src="{{ asset('assets/img/ambulance-xxl.png') }}" alt="urgence" class="imgHeader">
-                        <button class="btn btn-xs large" name="question">Poser ma question!</button>
 
                     </div>
                     </div>
 					</div>
-				</div>
 			</header>
 
         <div class="container">
@@ -71,7 +68,7 @@
 
 
  @include('partials._footer')
-
+          </div>
 
 	  </body>
 	</html>
