7b5213629f6525c3e979e2c835b2d9f5f32fade4
emili3 <emiliegachoud@MBP-de-Emilie-2.domain_not_set.invalid>
2016-06-13 21:47:24 +0200

    microData

--- resources/views/view_topic.blade.php
+++ resources/views/view_topic.blade.php
@@ -4,32 +4,32 @@
 
 @section('content')
 
-<div class="container article">
+<div class="container article" itemscope itemtype="https://schema.org/DiscussionForumPosting">
     <div class="row" id="contenu">
 
     <div class="col-md-12" id="breadcrums">
       <p>Accueil <span class="interBread">></span> {{$domain->name}} <span class="interBread">></span> Discussion</p>
     </div>
-        </div>
+    </div>
 
       <div class="col-md-7 designBox">
          <h2>Discussion</h2>
-                    <h3>{{$topic->name}}</h3>
+                    <h3 itemprop="about">{{$topic->name}}</h3>
                 <div class="forum">
                 <div class="divContainerQuestion">
-                        <label class="labelMessage">{{$topic->creatorUser->nickname}}</label>
+                        <label class="labelMessage" itemprop="creator">{{$topic->creatorUser->nickname}}</label>
 
-                        <label class="date">{{$topic->created_at}}</label>
+                        <label class="date"><time itemprop="dateCreated">{{$topic->created_at}}</time></label>
 
                     <p class="ContainerAnswerQuestion firstPost">premier post: {{$posts->first()->content}}</p>
                 </div>
 
                 @foreach($posts->splice(1) as $post)
                 <div class="divContainerAnswer rep">
-                    <label data-nickname="{{$post->writterUser->nickname}}" class="labelMessage">{{$post->writterUser->nickname}}</label>
+                    <label data-nickname="{{$post->writterUser->nickname}}" class="labelMessage" itemprop="creator">{{$post->writterUser->nickname}}</label>
 
-                    <label class="date">{{$post->created_at}}</label>
-                    <p class="ContainerAnswerQuestion">
+                    <label class="date"><time itemprop="dateCreated">{{$post->created_at}}</time></label>
+                    <p class="ContainerAnswerQuestion" itemprop="comment">
                       réponse: {{$post->content}}
                     </p>
                     <button type="button" class="btn btn-xs" id="answerThis">répondre</button>
