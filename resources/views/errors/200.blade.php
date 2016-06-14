2dd54416dbc7e364ac6f59d5422e8478e772f5f0
xavmer9 <xavier.mercier@heig-vd.ch>
2016-06-14 22:39:39 +0200

    error bello

--- resources/views/errors/200.blade.php
+++ resources/views/errors/200.blade.php
@@ -4,45 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Refresh" content="1; url=/home">
+        <meta http-equiv="Refresh" content="1; url={{$url or '/home'}}">
         <title>Ajouté avec succès</title>
 
+        <link href=" {{ asset('assets/css/error.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #ccc6c6;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-            a{
-                color:  #ff5a5f;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,42 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Page non trouvée</title>
 
+        <link href=" {{ asset('assets/css/error.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #ccc6c6;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-            a{
-                color:  #ff5a5f;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
