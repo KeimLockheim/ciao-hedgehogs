a21fb72e1f4425fab2ec5311b08b1c84e37859d1
KeimLockheim <anthoinema@gmail.com>
2016-06-14 02:50:03 +0200

    qerwteszrdjfukzgiluhoij
    dwqrewtezuiloij

--- app/Http/Controllers/SecretQuestionController.php
+++ app/Http/Controllers/SecretQuestionController.php
@@ -19,8 +19,7 @@ class SecretQuestionController extends Controller {
     }
     $user = User::where('nickname',$nickname)->first();
     $secretQuestion = $user->secretQuestion;
-    dd($secretQuestion);
-    return $secretQuestion;
+    return "{name:$secretQuestion->name}";
   }
 
   /**
